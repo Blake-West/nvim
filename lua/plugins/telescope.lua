36171571e810780b7bd485daa940ba6a2e8e32b5
Blake West <Blake.R.West@gmail.com>
2024-08-07 18:03:01 -0400

    fzf native integration, added options folder and keybinds updated

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-telescope/telescope.nvim", tag = "0.1.8",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
+      }
+    }
+  }
+}
