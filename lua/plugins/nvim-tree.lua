70992702f6c4ff085a2f445b37b9112b15b5055c
Blake West <Blake.R.West@gmail.com>
2024-08-23 10:01:40 -0400

    Added configuration of nvim tree as a test.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,7 +5,26 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
+  opts = {
+    view = { width = 60 },
+
+  },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      view = {
+        width = 40,
+      },
+      renderer = {
+        icons = {
+          show = {
+            modified = true,
+          },
+        },
+        highlight_modified = "all"
+      },
+    }
+
+
+
   end,
 }
