ee2fe7e06a370583cf5275bbbfef1d473e99ae02
Blake West <Blake.R.West@gmail.com>
2024-08-23 10:42:14 -0400

    Added controller for nvim-tree

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,7 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
