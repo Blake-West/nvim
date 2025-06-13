c1670c9183ca4748c16675e3d965b9645bdca095
Blake West <Blake.R.West@gmail.com>
2025-06-13 09:32:50 -0400

    Migrated to which-key for keybinds

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,16 +1,13 @@
-local key_map = vim.api.nvim_set_keymap
-local key_map_opts = { noremap = true, silent = true }
-
-key_map('i', "jk", "<Esc>", key_map_opts)
-
--- Treesitter keybinds
+local wk = require("which-key")
 local builtin = require("telescope.builtin")
 
-vim.keymap.set('n', "<leader>ff", builtin.find_files, {})
-vim.keymap.set('n', "<leader>fg", builtin.live_grep, {})
-vim.keymap.set('n', "<leader>fb", builtin.buffers, {})
-vim.keymap.set('n', "<leader>fh", builtin.help_tags, {})
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', key_map_opts)
-
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, key_map_opts)
-
+wk.add({
+  { "<leader>f", group = "Find (Telescope)" },
+  { "<leader>ff", builtin.find_files, desc = "Find Files" },
+  { "<leader>fg", builtin.live_grep, desc = "Live Grep" },
+  { "<leader>fb", builtin.buffers, desc = "Buffers" },
+  { "<leader>fh", builtin.help_tags, desc = "Help Tags" },
+  { "<leader>e", ":NvimTreeToggle<CR>", desc = "Toggle File Explorer" },
+  { "gd", vim.lsp.buf.definition, desc = "Go to Definition" },
+  { "jk", "<Esc>", desc = "Exit Insert Mode", mode = "i" },
+})
