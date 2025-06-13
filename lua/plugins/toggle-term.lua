c72cedf9495d8e1e7dd014be7189dc87ca34afec
Blake West <Blake.R.West@gmail.com>
2025-06-13 09:33:04 -0400

    Modifed toggle-term

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -1,5 +1,29 @@
 return {
   {
-    'akinsho/toggleterm.nvim', version = "*", config = true,
+    'akinsho/toggleterm.nvim', 
+    version = "*", 
+    config = function()
+      require("toggleterm").setup{
+        size = 20,
+        open_mapping = [[<c-\>]],
+        hide_numbers = true,
+        shade_terminals = true,
+        start_in_insert = true,
+        insert_mappings = true,
+        terminal_mappings = true,
+        persist_size = true,
+        direction = 'float', -- or 'horizontal', 'vertical', 'tab'
+        close_on_exit = true,
+        shell = vim.o.shell,
+        float_opts = {
+          border = 'curved',
+          winblend = 0,
+          highlights = {
+            border = "Normal",
+            background = "Normal",
+          }
+        }
+      }
+    end
   },
 }
