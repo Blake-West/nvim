34ecb591113bf07b80f83dc05f1a32d2585689b0
Blake West <blake.r.west@gmail.com>
2026-03-07 16:05:44 -0500

    Fixed lsp and direnv support

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,39 +1,29 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
     "hrsh7th/cmp-nvim-lsp",
   },
   config = function()
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    -- Suppress the deprecation warning
-    vim.notify = (function()
-      local original_notify = vim.notify
-      return function(msg, level, opts)
-        if msg and type(msg) == "string" and msg:find("lspconfig.*framework.*deprecated") then
-          return
-        end
-        return original_notify(msg, level, opts)
-      end
-    end)()
+    -- Apply cmp-nvim-lsp capabilities to ALL servers
+    vim.lsp.config('*', {
+      capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    })
 
-    require("lspconfig").pyright.setup({
-      capabilities = capabilities,
+    -- Configure individual servers (merges with defaults from nvim-lspconfig's lsp/ directory)
+    vim.lsp.config('pyright', {
       settings = {
         python = {
           analysis = {
             typeCheckingMode = "basic",
             autoSearchPaths = true,
             useLibraryCodeForTypes = true,
-            diagnositicMode = "workspace"
-          }
-        }
-      }
+            diagnosticMode = "workspace",
+          },
+        },
+      },
     })
-    
-    -- Add clangd configuration for C/C++
-    require('lspconfig').clangd.setup({
-      capabilities = capabilities,
+
+    vim.lsp.config('clangd', {
       cmd = {
         "clangd",
         "--background-index",
@@ -47,85 +37,72 @@ return {
         usePlaceholders = true,
         completeUnimported = true,
         clangdFileStatus = true,
-        fallbackFlags = {
-          "-I" .. vim.fn.getcwd() .. "/include",
-          "-I" .. vim.fn.getcwd() .. "/src",
-          "-std=c++23",  -- or whatever C++ standard you use
-        },
       },
-      filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
     })
 
-    -- Add Rust configuration
-    require('lspconfig').rust_analyzer.setup({
-      capabilities = capabilities,
+    vim.lsp.config('rust_analyzer', {
       settings = {
         ["rust-analyzer"] = {
           imports = {
-            granularity = {
-              group = "module",
-            },
+            granularity = { group = "module" },
             prefix = "self",
           },
           cargo = {
-            buildScripts = {
-              enable = true,
-            },
+            buildScripts = { enable = true },
           },
-          procMacro = {
-            enable = true
+          procMacro = { enable = true },
+          checkOnSave = true,
+          check = {
+            command = "clippy",
           },
-          checkOnSave = {
-            command = "clippy",  -- Use clippy for linting
+          diagnostics = {
+            enable = true,
           },
           inlayHints = {
-            enable = true,
-            showParameterNames = true,
-            parameterHintsPrefix = "<- ",
-            otherHintsPrefix = "=> ",
+            bindingModeHints = { enable = true },
+            chainingHints = { enable = true },
+            closingBraceHints = { enable = true },
+            closureReturnTypeHints = { enable = "with_block" },
+            lifetimeElisionHints = { enable = "skip_trivial" },
+            parameterHints = { enable = true },
+            typeHints = { enable = true },
           },
-        }
-      }
+        },
+      },
     })
+
+    -- nil_ls uses defaults from nvim-lspconfig (cmd = {'nil'}, filetypes = {'nix'})
+
+    -- Enable all desired servers (silently skips if binary not on PATH)
+    vim.lsp.enable({ 'pyright', 'clangd', 'rust_analyzer', 'nil_ls' })
 
     -- Set up keybindings when LSP attaches
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('UserLspConfig', {}),
       callback = function(ev)
         local opts = { buffer = ev.buf, silent = true }
-        
-        -- Go to definition
+
+        -- Enable inlay hints (VSCode-like type/parameter hints)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client and client:supports_method('textDocument/inlayHint') then
+          vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
+        end
+
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        
-        -- Show hover information
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        
-        -- Find references
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        
-        -- Rename symbol
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        
-        -- Code actions
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-        
-        -- Format document
-        vim.keymap.set('n', '<leader>f', function()
-          vim.lsp.buf.format { async = true }
-        end, opts)
-        
-        -- Show line diagnostics
+        vim.keymap.set('n', '<leader>lf', function()
+          vim.lsp.buf.format({ async = true })
+        end, { buffer = ev.buf, silent = true, desc = "LSP Format" })
         vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
-        
-        -- Go to next diagnostic
         vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-        
-        -- Go to previous diagnostic
         vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
       end,
     })
 
-    -- Configure Diagnostics Display
+    -- Configure diagnostics display
     vim.diagnostic.config({
       virtual_text = {
         prefix = '●',
@@ -142,5 +119,5 @@ return {
         prefix = '',
       },
     })
-  end
+  end,
 }
